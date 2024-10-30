27cf6d8f1166d51a6004d734a75c2f5f0d9a7c6f
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-10-30 10:21:50 -0500

    feat: add port enviroment variable

--- src/config/envs.config.ts
+++ src/config/envs.config.ts
@@ -5,4 +5,7 @@ export const ENVS = {
     MONGO_URI: env.get('MONGO_URI')
         .required()
         .asUrlString(),
+    PORT: env.get('PORT')
+        .default(3000)
+        .asPortNumber(),
 }
