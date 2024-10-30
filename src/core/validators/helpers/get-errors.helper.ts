01fee1afa39b98f6d2813a5697d9876e1c901cd7
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-10-30 13:15:12 -0500

    feat: add helper to extract errors from validation-error object

---
+++ src/core/validators/helpers/get-errors.helper.ts
@@ -0,0 +1,12 @@
+import { ValidationError } from "class-validator";
+
+export const getErrors = (errors: ValidationError[]): string[] => {
+    let errsArray: Array<string> = [];
+    for (let errObj of errors) {
+        const constraints = errObj.constraints;
+        const messages: Array<string> = Object.values(constraints as any);
+        for (let msg of messages)
+            errsArray.push(msg);
+    }
+    return errsArray;
+}
