e55a40b5d6cbabef1d6372a6b24ce1265f38513b
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-02 14:24:58 -0500

    feat: add user-service create function + user-model as DI

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { UserService } from "../services/user.service";
 import { UserController } from "../controllers/user.controller";
+import { UserModel } from "../databases/mongo/schemas/user.schema";
 
 export class UserRoutes {
 
     static get routes(): Router {
         const router = Router();
-        const service = new UserService();
+        const service = new UserService(UserModel);
         const controller = new UserController(service);
         router.post('/', controller.create);
         return router;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,7 +1,26 @@
+import { CreateUserValidator } from "../rules/validators/create-user.validator";
+import { HttpError } from "../rules/errors/http.error";
+import { Model } from "mongoose";
+import { UserRequest } from "../types/user/user-request.type";
 
 export class UserService {
 
-    async create() {
-        return 'create user here';
+    constructor(
+        private readonly userModel: Model<UserRequest>
+    ){}
+
+    async create(user: CreateUserValidator): Promise<UserRequest>{
+        try {
+            const created = await this.userModel.create(user);
+            return created;
+        } catch (error: any) {            
+            if (error.code == 11000)
+                throw HttpError.badRequest(`user with name ${user.name} already exists`);
+            else {
+                // TODO: logs-service
+                console.log(error.message);
+                throw HttpError.internalServer(`Unexpected error`);
+            }
+        }
     }
 }
