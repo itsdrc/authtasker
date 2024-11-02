ba0c73a4d9657c36178f7941fd1e194e85425e28
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-02 14:26:23 -0500

    feat: connect to db and start server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
+import { ENVS } from "./config/envs.config";
+import { MongoDatabase } from "./databases/mongo/mongo.database";
+import { AppRoutes } from "./routes/server.routes";
+import { Server } from "./server/server.init";
 
-function main(){
-    console.log('Hello World');
+async function main() {    
+    await MongoDatabase.connect(ENVS.MONGO_URI);
+    const server = new Server(ENVS.PORT,AppRoutes.routes,);
+    server.start();
 }
 
 main();
