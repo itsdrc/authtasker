98ad54e15e7167e0f5200502e223d092d94eea6f
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-03 22:06:18 -0500

    feat: add login function to user-controller class

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,6 +3,9 @@ import { UserService } from "../services/user.service"
 import { CreateUserValidator } from "../rules/validators/create-user.validator";
 import { HttpError } from "../rules/errors/http.error";
 import { handleError } from "./helpers/handle-error.helper";
+import { LoginUserValidator } from "../rules/validators/login-user.validator";
+
+// TODO: change plain numbers for error codes
 
 export class UserController {
 
@@ -12,7 +15,7 @@ export class UserController {
 
     readonly create = async (req: Request, res: Response): Promise<void> => {
         const user = req.body;
-        const [errors, validatedUser] = await CreateUserValidator.create(user);
+        const [errors, validatedUser] = await CreateUserValidator.validateAndTransform(user);
 
         if (errors) {
             res.status(400).json({ errors });
@@ -22,6 +25,23 @@ export class UserController {
         try {
             const created = await this.userService.create(validatedUser);
             res.status(200).json(created);
+        } catch (error) {
+            handleError(res, error);
+        }
+    }
+
+    readonly login = async (req: Request, res: Response): Promise<void> => {
+        const user = req.body;
+        const [errors, validatedUser] = await LoginUserValidator.validate(user);
+
+        if (errors) {
+            res.status(400).json({ errors });
+            return;
+        }
+
+        try {
+            const loggedIn = await this.userService.login(validatedUser);
+            res.status(200).json(loggedIn);
         } catch (error) {
             handleError(res, error);
         }
