a48f9558db2f9f24879d8816a09c1e71195205bb
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-03 21:39:53 -0500

    feat: add jwt-service

---
+++ src/services/jwt-token.service.ts
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+export class JwtService {
+
+    constructor(
+        private readonly expirationTime: string,
+        private readonly privateKey: string,
+    ) {}
+
+    generate(payload: object): string {
+        const token = jwt.sign(payload,
+            this.privateKey,
+            { expiresIn: this.expirationTime }
+        );
+        return token;
+    }
+
+    verify(token: string): object | string{
+        const payload = jwt.verify(token, this.privateKey);
+        return payload;
+    }
+}
