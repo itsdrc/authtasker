869203c91ef476958c38cee3ef233398f95d5ca5
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-03 22:05:33 -0500

    feat: add function to login user to user-service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -4,6 +4,7 @@ import { Model } from "mongoose";
 import { UserRequest } from "../types/user/user-request.type";
 import { HashingService } from "./hashing.service";
 import { JwtService } from "./jwt-token.service";
+import { LoginUserValidator } from "../rules/validators/login-user.validator";
 
 export class UserService {
 
@@ -35,6 +36,31 @@ export class UserService {
                 console.log(error.message);
                 throw HttpError.internalServer(`Unexpected error`);
             }
+        }
+    }
+
+    async login(userToLogin: LoginUserValidator): Promise<{ user: UserRequest, token: string }> {
+
+        const userDb = await this.userModel.findOne({
+            email: userToLogin.email
+        });
+
+        if (!userDb)
+            throw HttpError.badRequest(`User with email ${userToLogin.email} not found`);
+
+        const passwordOk = await this.hashingService.compare(
+            userToLogin.password,
+            userDb.password
+        );
+
+        if(!passwordOk)
+            throw HttpError.badRequest('Incorrect password');
+
+        const token = this.jwtService.generate({id: userDb.id});
+
+        return {
+            user: userDb,
+            token,
         }
     }
 }
