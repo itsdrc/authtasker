511223716bed6b28ae4775d6c7d30bb31df9db4b
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-03 20:01:29 -0500

    feat: add bcrypt_salt_rounds env

--- src/config/envs.config.ts
+++ src/config/envs.config.ts
@@ -8,4 +8,7 @@ export const ENVS = {
     PORT: env.get('PORT')
         .default(3000)
         .asPortNumber(),
+    BCRYPT_SALT_ROUNDS: env.get('BCRYPT_SALT_ROUNDS')
+        .default(10)
+        .asInt(),
 }
