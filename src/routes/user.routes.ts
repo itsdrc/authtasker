f1750d43ed0234e5f2611b32311a49d38aedc291
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-03 20:01:49 -0500

    feat: hash user password before saving

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,12 +2,18 @@ import { Router } from "express";
 import { UserService } from "../services/user.service";
 import { UserController } from "../controllers/user.controller";
 import { UserModel } from "../databases/mongo/schemas/user.schema";
+import { HashingService } from "../services/hashing.service";
+import { ENVS } from "../config/envs.config";
 
 export class UserRoutes {
 
     static get routes(): Router {
-        const router = Router();
-        const service = new UserService(UserModel);
+        const router = Router();        
+        const hashingService = new HashingService(ENVS.BCRYPT_SALT_ROUNDS);
+        const service = new UserService(
+            UserModel,
+            hashingService
+        );
         const controller = new UserController(service);
         router.post('/', controller.create);
         return router;
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,17 +2,24 @@ import { CreateUserValidator } from "../rules/validators/create-user.validator";
 import { HttpError } from "../rules/errors/http.error";
 import { Model } from "mongoose";
 import { UserRequest } from "../types/user/user-request.type";
+import { HashingService } from "./hashing.service";
 
 export class UserService {
 
     constructor(
-        private readonly userModel: Model<UserRequest>
+        private readonly userModel: Model<UserRequest>,
+        private readonly hashingService: HashingService,
     ){}
 
     // TODO: hash password, validate email and return token
     async create(user: CreateUserValidator): Promise<UserRequest>{
         try {
+            const passwordHash = await this.hashingService.hash(user.password);
+
+            user.password = passwordHash;
+
             const created = await this.userModel.create(user);
+            
             return created;
         } catch (error: any) {            
             if (error.code == 11000)
