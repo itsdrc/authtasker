c647632fb191c4274ff571814db265d1460dab28
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-03 23:53:49 -0500

    chore: change it for test word in tests

--- src/tests/rules/validators/create-user.validator.test.ts
+++ src/tests/rules/validators/create-user.validator.test.ts
@@ -12,13 +12,14 @@ describe('CreateUserValidator', () => {
     } as const;
 
     describe('validation successful', () => {
-        it('should validate the user successfully', async () => {
+        test('should validate the user successfully', async () => {
             const [error, userValidated] = await CreateUserValidator.validateAndTransform(user);
             expect(error).toBeUndefined();
+            expect(user).toEqual(userValidated);
             expect(Object.keys(user)).toStrictEqual(Object.keys(userValidated as object));
         });
 
-        it('should transform the name to lowercase and trim it', async () => {
+        test('should transform the name to lowercase and trim it', async () => {
             const [, userValidated] = await CreateUserValidator.validateAndTransform(user);
             expect(userValidated?.name).toBe(user.name.toLowerCase().trim());
         });
@@ -30,7 +31,7 @@ describe('CreateUserValidator', () => {
         const minNameLength = USER_VALIDATION_CONSTANTS.MIN_NAME_LENGTH;
         const minPasswordLength = USER_VALIDATION_CONSTANTS.MIN_PASSWORD_LENGTH;
 
-        it(`should fail if name length is greater than ${maxNameLength}`, async () => {
+        test(`should fail if name length is greater than ${maxNameLength}`, async () => {
             const invalidUser: CreateUserValidator = structuredClone(user);
             invalidUser.name = createString(maxNameLength + 1);
             const [error] = await CreateUserValidator.validateAndTransform(invalidUser);
@@ -38,7 +39,7 @@ describe('CreateUserValidator', () => {
             expect(error).toStrictEqual([`name must be shorter than or equal to ${maxNameLength} characters`]);
         });
 
-        it(`should fail if name length is less than ${minNameLength}`, async () => {
+        test(`should fail if name length is less than ${minNameLength}`, async () => {
             const invalidUser: CreateUserValidator = structuredClone(user);
             invalidUser.name = createString(minNameLength - 1);
             const [error] = await CreateUserValidator.validateAndTransform(invalidUser);
@@ -46,7 +47,7 @@ describe('CreateUserValidator', () => {
             expect(error).toStrictEqual([`name must be longer than or equal to ${minNameLength} characters`]);
         });
 
-        it(`should fail if email is not a valid email`, async () => {
+        test(`should fail if email is not a valid email`, async () => {
             const invalidUser: CreateUserValidator = structuredClone(user);
             invalidUser.email = "invalid-email";
             const [error] = await CreateUserValidator.validateAndTransform(invalidUser);
@@ -54,7 +55,7 @@ describe('CreateUserValidator', () => {
             expect(error).toStrictEqual(['email must be an email']);
         });
 
-        it(`should fail if password length is greater than ${maxPasswordLength}`, async () => {
+        test(`should fail if password length is greater than ${maxPasswordLength}`, async () => {
             const invalidUser: CreateUserValidator = structuredClone(user);
             invalidUser.password = createString(maxPasswordLength + 1);
             const [error] = await CreateUserValidator.validateAndTransform(invalidUser);
@@ -62,7 +63,7 @@ describe('CreateUserValidator', () => {
             expect(error).toStrictEqual([`password must be shorter than or equal to ${maxPasswordLength} characters`]);
         });
 
-        it(`should fail if password length is less than ${minPasswordLength}`, async () => {
+        test(`should fail if password length is less than ${minPasswordLength}`, async () => {
             const invalidUser: CreateUserValidator = structuredClone(user);
             invalidUser.password = createString(minPasswordLength - 1);
             const [error] = await CreateUserValidator.validateAndTransform(invalidUser);
@@ -70,7 +71,7 @@ describe('CreateUserValidator', () => {
             expect(error).toStrictEqual([`password must be longer than or equal to ${minPasswordLength} characters`]);
         });
 
-        it(`should fail if role is not a valid role`, async () => {
+        test(`should fail if role is not a valid role`, async () => {
             const invalidUser: CreateUserValidator = structuredClone(user);
             invalidUser.role = 'new-role' as any;
             const [error] = await CreateUserValidator.validateAndTransform(invalidUser);
@@ -78,7 +79,7 @@ describe('CreateUserValidator', () => {
             expect(error).toStrictEqual([`role must be one of the following values: ${validRoles.join(', ')}`]);
         });
 
-        it('should fail if there are unexpected properties in the object', async()=>{
+        test('should fail if there are unexpected properties in the object', async()=>{
             const newProperty = 'unknownProperty';
             const invalidUser: any= structuredClone(user);
             invalidUser[newProperty] = 10;            
