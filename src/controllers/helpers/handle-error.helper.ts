91b691abb783fb38cc88bb7f5d86ee4068a4ede7
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-04 21:55:12 -0500

    feat: change unhandled-error message in handle-error helper

--- src/controllers/helpers/handle-error.helper.ts
+++ src/controllers/helpers/handle-error.helper.ts
@@ -1,9 +1,10 @@
 import { Response } from "express";
 import { HttpError } from "../../rules/errors/http.error";
 
+// TODO: logs-service
 export const handleError = (res:Response, error: unknown) => {
     if (error instanceof HttpError)
         res.status(error.statusCode).json({ error: error.message });
     else
-        res.status(500).json({ unhandledError: error });
+        res.status(500).json({ error: 'Unexpected error' });
 };
