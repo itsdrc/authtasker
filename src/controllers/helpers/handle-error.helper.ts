7ec69febc5c9c223f6bfbb67d0ee121483c851a9
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-10 13:41:25 -0500

    feat: log errors on console (in the meantime)

--- src/controllers/helpers/handle-error.helper.ts
+++ src/controllers/helpers/handle-error.helper.ts
@@ -5,6 +5,8 @@ import { HttpError } from "../../rules/errors/http.error";
 export const handleError = (res:Response, error: unknown) => {
     if (error instanceof HttpError)
         res.status(error.statusCode).json({ error: error.message });
-    else
+    else{
+        console.error(error);
         res.status(500).json({ error: 'Unexpected error' });
+    }
 };
