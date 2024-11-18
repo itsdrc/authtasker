6dddba2eab94bc633ecc98e04cdb73d032b27a09
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-17 21:49:21 -0500

    fix: use return-await in mongo-database-class to improve the stack trace

--- src/databases/mongo/mongo.database.ts
+++ src/databases/mongo/mongo.database.ts
@@ -3,11 +3,11 @@ import mongoose from "mongoose";
 export class MongoDatabase {
 
     // TODO: envs to set timeout ms
-    static connect(mongoURI: string): Promise<mongoose.Mongoose> {
-        return mongoose.connect(mongoURI, { serverSelectionTimeoutMS: 10000 });        
+    static async connect(mongoURI: string): Promise<mongoose.Mongoose> {
+        return await mongoose.connect(mongoURI, { serverSelectionTimeoutMS: 10000 });
     }
 
-    static disconnect(): Promise<void> {
-        return mongoose.disconnect();
+    static async disconnect(): Promise<void> {
+        return await mongoose.disconnect();
     }
 }
