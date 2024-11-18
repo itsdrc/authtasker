311e31c0233663ba68ed39ca44a9d4b02a33fdee
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-18 10:59:55 -0500

    feat: return promises for start and close server functions

--- src/server/server.init.ts
+++ src/server/server.init.ts
@@ -1,19 +1,39 @@
 import express, { Router } from "express";
+import { Server as HttpServer } from "http";
 
 export class Server {
 
     private readonly app = express();
+    private server: HttpServer | undefined;
 
     constructor(
         private readonly port: number,
         private readonly routes: Router,
     ) {}
 
-    start(): void {
+    start(): Promise<void> {
         this.app.use(express.json());
         this.app.use(this.routes);
-        this.app.listen(this.port, () => {
-            console.log(`Listening on port ${this.port}`);
-        })
+
+        return new Promise<void>((resolve) => {
+            this.server = this.app.listen(this.port, () => {
+                console.log(`Server listening on port ${this.port}`);
+                resolve();
+            });
+        });
+    }
+
+    close(): Promise<void> {
+        return new Promise<void>((resolve) => {
+            if (this.server) {
+                this.server.closeAllConnections();
+                this.server.close(() => {
+                    console.log('Server closed');
+                    resolve();
+                });
+            } else {
+                resolve();
+            }
+        });
     }
 }
