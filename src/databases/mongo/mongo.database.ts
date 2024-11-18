7113e4fb2d94357b8dd14fc0f10a6e7bcedcc0e2
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-17 21:47:58 -0500

    feat: use a custom timeout for mongoose reconnection

--- src/databases/mongo/mongo.database.ts
+++ src/databases/mongo/mongo.database.ts
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 export class MongoDatabase {
 
+    // TODO: envs to set timeout ms
     static connect(mongoURI: string): Promise<mongoose.Mongoose> {
-        return mongoose.connect(mongoURI);
+        return mongoose.connect(mongoURI, { serverSelectionTimeoutMS: 10000 });        
     }
 
     static disconnect(): Promise<void> {
