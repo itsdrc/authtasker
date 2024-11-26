c80835fc7c970025c0713df7648989076527a738
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-11-26 17:33:46 -0500

    feat: update get-error.helper to be handle errors properly

--- src/rules/validators/helpers/get-error.helper.ts
+++ src/rules/validators/helpers/get-error.helper.ts
@@ -1,22 +1,26 @@
 import { ValidationError } from "class-validator";
 
 export const getError = (errors: ValidationError[]): string => {
-    let whitelistError = false;
+    let allWhiteListValidationError = false;
 
     let errsArray: Array<string> = [];
     for (let errObj of errors) {
         const constraints = errObj.constraints;
-        if (Object.keys(constraints || {}).includes('whitelistValidation'))
-            whitelistError = true;
-        const messages: Array<string> = Object.values(constraints as any);
-        for (let msg of messages)
-            errsArray.push(msg);
+
+        const causes = Object.keys(constraints || {});
+        allWhiteListValidationError = causes.every(err => err === 'whitelistValidation');
+
+        if (constraints) {
+            const messages: Array<string> = Object.values(constraints);
+            for (let msg of messages)
+                errsArray.push(msg);
+        }
     }
 
     if (errsArray.length > 1) {
         // If more than one unexpected property is in the object, 
         // always returns the first one.
-        if (whitelistError)
+        if (allWhiteListValidationError)
             return errsArray[0];
         else
             throw new Error('More than one error was found, which was not expected')
