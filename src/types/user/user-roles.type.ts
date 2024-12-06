320551f1d2ba33816dbcda150a5e9202f6c0b75a
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-12-06 13:08:18 -0500

    feat: add new role: editor

--- src/types/user/user-roles.type.ts
+++ src/types/user/user-roles.type.ts
@@ -1,5 +1,5 @@
 
 // Represents the user roles.
 // validRoles is useful for validation.
-export const validRoles = ['admin', 'readonly'] as const;
+export const validRoles = ['admin', 'readonly', 'editor'] as const;
 export type UserRoles = typeof validRoles[number];
