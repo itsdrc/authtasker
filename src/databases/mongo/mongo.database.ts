287c649e84b1b7e5dcf561eefece77cba34bc844
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-12-10 11:11:11 -0500

    chore: remove todo comment in mongo db class

--- src/databases/mongo/mongo.database.ts
+++ src/databases/mongo/mongo.database.ts
@@ -1,12 +1,10 @@
-import { SystemLoggerService } from "@root/services/system-logger.service";
 import mongoose from "mongoose";
 
 export class MongoDatabase {
 
-    // TODO: envs to set timeout ms
     static async connect(mongoURI: string): Promise<boolean> {
         try {
-            await mongoose.connect(mongoURI, { serverSelectionTimeoutMS: 1000 });
+            await mongoose.connect(mongoURI);
             return true;
         } catch (error) {
             return false;
