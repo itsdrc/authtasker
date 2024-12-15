01bc214abba91daa2904e0b8fefd4e6465bdcd28
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-12-14 23:51:21 -0500

    fix: e2e test teardown

--- test/e2e/config/teardown.ts
+++ test/e2e/config/teardown.ts
@@ -5,7 +5,7 @@ import { SystemLoggerService } from "@root/services/system-logger.service";
 export default async ()=>{
     await MongoDatabase.connect(process.env.MONGO_URI!);
     const userModel = loadUserModel({} as any);
-    userModel.deleteMany().exec();
+    await userModel.deleteMany().exec();
     await MongoDatabase.disconnect();
     SystemLoggerService.warn('All db documents deleted');
 }
