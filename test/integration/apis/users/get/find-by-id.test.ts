7897a7875a98a62d538313024fa77a7ecd1a3337
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2024-12-27 19:01:47 -0500

    test (int): add GET/ find user by id tests

---
+++ test/integration/apis/users/get/find-by-id.test.ts
@@ -0,0 +1,95 @@
+import request from 'supertest'
+import { Types } from 'mongoose';
+import { HashingService } from '@root/services';
+import { generateValidTokenWithId } from '../../../helpers/token/generate-valid-token-with-id';
+
+describe('GET/', () => {
+    const hashingService = new HashingService(+process.env.BCRYPT_SALT_ROUNDS!);
+
+    describe('Find by id', () => {
+        describe('User is found', () => {
+            test('should return: right user, right data and 200 OK', async () => {
+                const expectedStatus = 200;
+
+                // create a user
+                const userInDb = await global.USER_MODEL.create({
+                    name: global.USER_DATA_GENERATOR.name(),
+                    email: global.USER_DATA_GENERATOR.email(),
+                    password: await hashingService.hash(global.USER_DATA_GENERATOR.password()),
+                });
+
+                // generate a token with user id 
+                const token = generateValidTokenWithId(userInDb.id);
+
+                const response = await request(global.SERVER_APP)
+                    .get(`${global.USERS_PATH}/${userInDb.id}`)
+                    .set('Authorization', `Bearer ${token}`)
+                    .expect(expectedStatus);
+
+                expect(response.body).toStrictEqual({
+                    name: userInDb.name,
+                    email: userInDb.email,
+                    role: userInDb.role,
+                    emailValidated: userInDb.emailValidated,
+                    createdAt: userInDb.createdAt.toISOString(),
+                    updatedAt: userInDb.updatedAt.toISOString(),
+                    id: userInDb.id,
+                });
+            });
+        });
+
+        // ensures roles middlewares is applied
+        describe('Token not provided', () => {
+            test('return status 401 UNAUTHORIZED', async () => {
+                const expectedStatus = 401;
+                const anyID = 123;
+
+                await request(global.SERVER_APP)
+                    .get(`${global.USERS_PATH}/${anyID}`)
+                    .expect(expectedStatus);
+            });
+        });
+
+        describe('Id is not valid', () => {
+            test('return status 404 NOT FOUND', async () => {
+                const expectedStatus = 404;
+
+                // create a user in order to use their id to generate a token                
+                const userInDb = await global.USER_MODEL.create({
+                    name: global.USER_DATA_GENERATOR.name(),
+                    email: global.USER_DATA_GENERATOR.email(),
+                    password: await hashingService.hash(global.USER_DATA_GENERATOR.password()),
+                });
+                const token = generateValidTokenWithId(userInDb.id);
+
+                const invalidMongoId = 123;
+
+                await request(global.SERVER_APP)
+                    .get(`${global.USERS_PATH}/${invalidMongoId}`)
+                    .set('Authorization', `Bearer ${token}`)
+                    .expect(expectedStatus);
+            });
+        });
+
+        describe('User is not found', () => {
+            test('return status 404 NOT FOUND', async () => {
+                const expectedStatus = 404;
+
+                // create a user in order to use their id to generate a token                
+                const userInDb = await global.USER_MODEL.create({
+                    name: global.USER_DATA_GENERATOR.name(),
+                    email: global.USER_DATA_GENERATOR.email(),
+                    password: await hashingService.hash(global.USER_DATA_GENERATOR.password()),
+                });
+                const token = generateValidTokenWithId(userInDb.id);
+
+                const validMongoId = new Types.ObjectId();
+
+                await request(global.SERVER_APP)
+                    .get(`${global.USERS_PATH}/${validMongoId}`)
+                    .set('Authorization', `Bearer ${token}`)
+                    .expect(expectedStatus);
+            });
+        });
+    });
+});
