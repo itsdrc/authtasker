78f8124e6c2814ffc61e5675efc2075ef85725d2
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-01-16 11:01:52 -0500

    test (unit): add tests for token blacklisting in roles middleware

--- test/unit/middlewares/roles.middleware.test.ts
+++ test/unit/middlewares/roles.middleware.test.ts
@@ -4,7 +4,7 @@ import express, { Request, Response } from "express";
 import { v4 as uuidv4 } from 'uuid';
 import request from 'supertest'
 
-import { JwtService, LoggerService } from '@root/services';
+import { JwtBlackListService, JwtService, LoggerService } from '@root/services';
 import { IUser } from '@root/interfaces/user/user.interface';
 import { FindMockQuery } from '../helpers/types/find-mock-query.type';
 import { rolesMiddlewareFactory } from '@root/middlewares/roles.middleware';
@@ -16,6 +16,7 @@ import { TOKEN_PURPOSES } from '@root/rules/constants/token-purposes.constants';
 describe('Roles middleware', () => {
     let userModel: FindMockQuery<MockProxy<Model<IUser>>>;
     let loggerService: MockProxy<LoggerService>;
+    let jwtBlackListService: MockProxy<JwtBlackListService>;
     const jwtService = new JwtService('12345test');
 
     const app = express();
@@ -23,6 +24,7 @@ describe('Roles middleware', () => {
     beforeEach(() => {
         userModel = (mock<Model<IUser>>() as any);
         loggerService = mock<LoggerService>();
+        jwtBlackListService = mock<JwtBlackListService>();
         userModel.findById.mockReturnValue(mock<Query<any, any>>());
     });
 
@@ -37,7 +39,8 @@ describe('Roles middleware', () => {
                 'readonly',
                 userModel as unknown as Model<IUser>,
                 loggerService,
-                jwtService
+                jwtService,
+                jwtBlackListService
             );
 
             app.get(endpoint, middleware, (req, res) => {
@@ -63,7 +66,8 @@ describe('Roles middleware', () => {
                 'readonly',
                 userModel as unknown as Model<IUser>,
                 loggerService,
-                jwtService
+                jwtService,
+                jwtBlackListService
             );
 
             app.get(endpoint, middleware, (req, res) => {
@@ -99,7 +103,8 @@ describe('Roles middleware', () => {
                 'readonly',
                 userModel as unknown as Model<IUser>,
                 loggerService,
-                jwtService
+                jwtService,
+                jwtBlackListService
             );
 
             app.get(endpoint, middleware, (req, res) => {
@@ -111,6 +116,48 @@ describe('Roles middleware', () => {
                 id: '12345',
                 purpose: TOKEN_PURPOSES.SESSION,
             });
+
+            // stub blacklist
+            jwtBlackListService.isBlacklisted
+                .mockResolvedValue(false);
+
+            const response = await request(app)
+                .get(endpoint)
+                .set('Authorization', `Bearer ${validToken}`);
+
+            expect(response.statusCode).toBe(expectedStatus);
+            expect(response.body.error).toBe(expectedMessage);
+        });
+    });
+
+    describe('Token is blacklisted', () => {
+        test('should return 401 UNAUTHORIZED, "Invalid bearer token"', async () => {
+            // test endpoint
+            const endpoint = `/test/${uuidv4()}/readonly`;
+            const expectedStatus = 401;
+            const expectedMessage = 'Invalid bearer token';
+
+            const middleware = rolesMiddlewareFactory(
+                'readonly',
+                userModel as unknown as Model<IUser>,
+                loggerService,
+                jwtService,
+                jwtBlackListService
+            );
+
+            app.get(endpoint, middleware, (req, res) => {
+                res.status(204).end();
+            });
+
+            // generate a valid token
+            const validToken = jwtService.generate('1m', {
+                id: '12345',
+                purpose: TOKEN_PURPOSES.SESSION,
+            });
+
+            // mock blacklist
+            jwtBlackListService.isBlacklisted
+                .mockResolvedValue(true);
 
             const response = await request(app)
                 .get(endpoint)
@@ -132,7 +179,8 @@ describe('Roles middleware', () => {
                 'readonly',
                 userModel as unknown as Model<IUser>,
                 loggerService,
-                jwtService
+                jwtService,
+                jwtBlackListService
             );
 
             app.get(endpoint, middleware, (req, res) => {
@@ -144,6 +192,10 @@ describe('Roles middleware', () => {
                 id: '12345',
                 purpose: TOKEN_PURPOSES.SESSION,
             });
+
+            // stub blacklist 
+            jwtBlackListService.isBlacklisted
+                .mockResolvedValue(false);
 
             // stub user found
             userModel
@@ -171,7 +223,8 @@ describe('Roles middleware', () => {
                 'readonly',
                 userModel as unknown as Model<IUser>,
                 loggerService,
-                jwtService
+                jwtService,
+                jwtBlackListService
             );
 
             const requestMock = mock<Request>();
@@ -182,14 +235,18 @@ describe('Roles middleware', () => {
 
             // stub res.status().json();
             responseMock.status.mockReturnThis();
-            
+
             // stub token verification
             const requestUserId = 'test-id-123';
             const tokenVerificationMock = jest.spyOn(jwtService, 'verify')
-                .mockReturnValue({ 
+                .mockReturnValue({
                     id: requestUserId,
                     purpose: TOKEN_PURPOSES.SESSION,
                 } as any);
+
+            // stub blacklist
+            jwtBlackListService.isBlacklisted
+                .mockResolvedValue(false);
 
             // mock user found, obviously, the same as in the token payload
             const userFound = {
@@ -223,7 +280,8 @@ describe('Roles middleware', () => {
                 'readonly',
                 userModel as unknown as Model<IUser>,
                 loggerService,
-                jwtService
+                jwtService,
+                jwtBlackListService
             );
 
             const requestMock = mock<Request>();
@@ -248,6 +306,10 @@ describe('Roles middleware', () => {
                 .findById()
                 .exec
                 .mockResolvedValue({} as any);
+
+            // stub blacklist
+            jwtBlackListService.isBlacklisted
+                .mockResolvedValue(false);
 
             // mock canAccess helper
             const canAccessHelperMock = jest.spyOn(MiddlewareHelpers, 'canAccess')
@@ -274,7 +336,8 @@ describe('Roles middleware', () => {
                 'readonly',
                 userModel as unknown as Model<IUser>,
                 loggerService,
-                jwtService
+                jwtService,
+                jwtBlackListService
             );
 
             const requestMock = mock<Request>();
@@ -294,6 +357,10 @@ describe('Roles middleware', () => {
                     purpose: TOKEN_PURPOSES.SESSION,
                 } as any);
 
+            // stub blacklist
+            jwtBlackListService.isBlacklisted
+                .mockResolvedValue(false);
+
             // mock findById().exec() to cause an error
             userModel
                 .findById()
@@ -308,5 +375,53 @@ describe('Roles middleware', () => {
 
             expect(handleErrorHelperSpy).toHaveBeenCalled();
         });
+    });
+
+    test('jwtBlacklistService.isBlacklisted should be called with jti (in payload)', async () => {
+        const middleware = rolesMiddlewareFactory(
+            'readonly',
+            userModel as unknown as Model<IUser>,
+            loggerService,
+            jwtService,
+            jwtBlackListService
+        );
+
+        const requestMock = mock<Request>();
+        const responseMock = mock<Response>();
+        const nextFunctionMock = jest.fn();
+
+        // stub token provided
+        requestMock.header.mockReturnValue('something');
+
+        // stub res.status().json();
+        responseMock.status.mockReturnThis();
+
+        // mock token verification
+        const testJti = 'test jti';
+        jest.spyOn(jwtService, 'verify')
+            .mockReturnValue({
+                id: '123',
+                purpose: TOKEN_PURPOSES.SESSION,
+                jti: testJti
+            } as any);
+
+        // stub blacklist
+        jwtBlackListService.isBlacklisted
+            .mockResolvedValue(false);
+
+        // stub findById().exec()
+        userModel
+            .findById()
+            .exec
+            .mockResolvedValue({});
+
+        await middleware(
+            requestMock,
+            responseMock,
+            nextFunctionMock
+        );
+
+        expect(jwtBlackListService.isBlacklisted)
+            .toHaveBeenCalledWith(testJti);
     });
 });
