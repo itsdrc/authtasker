acb571b2f10c8fdd644c61e980e49feabfb3a8de
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-01-17 21:14:35 -0500

    test (e2e): add test to ensure token (in url) is blacklisted after email validation

--- test/e2e/apis/authentication/email-validation.test.ts
+++ test/e2e/apis/authentication/email-validation.test.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { ImapFlow, MailboxLockObject } from "imapflow";
 import { handleAxiosError } from "../../helpers/handlers/axios-error.handler";
 import { getUrlFromEmailMessage } from "../../helpers/email/get-url-from-message.helper";
@@ -25,6 +25,11 @@ describe('Authentication', () => {
 
         afterAll(async () => {
             await client.logout();
+        });
+
+        // close the inbox in order to refresh it the next time is opened
+        afterEach(async () => {
+            await client.mailboxClose();
         });
 
         test('user should become an "editor" and emailValidated should be true', async () => {
@@ -81,6 +86,66 @@ describe('Authentication', () => {
 
                 expect(userFoundResponse.data.role).toBe(finalRole);
                 expect(userFoundResponse.data.emailValidated).toBeTruthy();
+
+                lock.release();
+
+            } catch (error) {
+                if (lock)
+                    lock.release();
+                handleAxiosError(error);
+            }
+        }, 15000);
+
+        test('can not confirm email twice (token is blacklisted) (400 BAD REQUEST)', async () => {
+            const expectedStatus = 400;
+            let lock: MailboxLockObject | undefined;
+
+            try {
+                // create user
+                const user = {
+                    name: global.DATA_GENERATOR.name(),
+                    email: global.DATA_GENERATOR.email(),
+                    password: global.DATA_GENERATOR.password()
+                };
+
+                const createdUserResponse = await axios.post(
+                    global.REGISTER_USER_PATH,
+                    user
+                );
+
+                const userToken = createdUserResponse.data.token;
+                const userId = createdUserResponse.data.user.id;
+
+                // request email validation
+                await axios.post(global.EMAIL_VALIDATION_PATH, {}, {
+                    headers: {
+                        Authorization: `Bearer ${userToken}`
+                    }
+                });
+
+                // get the link sent to user's email
+                lock = await client.getMailboxLock('INBOX');
+                const messages = await client.search({ to: user.email });
+                const message = await client.fetchOne(`${messages.at(-1)}`, { bodyParts: ['1'] });
+                const rawEmailMessage = message.bodyParts
+                    .values()
+                    .next()
+                    .value
+                    ?.toString()!;
+                const confirmEmailUrl = getUrlFromEmailMessage(rawEmailMessage);
+
+                // go to the link sent to email to finish the email validation
+                await axios.get(confirmEmailUrl);
+
+                try {
+                    // try to confirm email validation again, the same token
+                    await axios.get(confirmEmailUrl);
+                    expect(true).toBeFalsy();
+
+                } catch (error) {
+                    const axiosError = error as AxiosError;
+                    expect(axiosError.response?.status).toBe(expectedStatus);
+                }
 
                 lock.release();
 
