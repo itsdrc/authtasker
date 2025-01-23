5d9f297ccba655c0a9bd9d1610a64897ebfbe783
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-01-23 15:51:50 -0500

    test (e2e): add config for tasks endpoint tests

--- test/e2e/configuration/test-file/setupAfterEnv.ts
+++ test/e2e/configuration/test-file/setupAfterEnv.ts
@@ -2,12 +2,14 @@ import { UserDataGenerator } from "@root/seed/generators/user.generator";
 import { SystemLoggerService } from "@root/services/system-logger.service";
 import { readFileSync } from "fs";
 import { readAdminToken } from "../../helpers/admin/token/read-admin-token.helper";
+import { TasksDataGenerator } from "@root/seed/generators/tasks.generator";
 
 beforeAll(() => {
     // disable info logs during tests
     jest.spyOn(SystemLoggerService, 'info').mockImplementation();
 
     const dataGenerator = new UserDataGenerator();
+    const tasksDataGenerator = new TasksDataGenerator();
 
     global.ADMIN_TOKEN = readAdminToken();
     global.REGISTER_USER_PATH = `${process.env.WEB_URL}/api/users/create`;
@@ -15,4 +17,7 @@ beforeAll(() => {
     global.USERS_PATH = `${process.env.WEB_URL}/api/users`;
     global.EMAIL_VALIDATION_PATH = `${process.env.WEB_URL}/api/users/requestEmailValidation`;
     global.DATA_GENERATOR = dataGenerator;
+    global.TASKS_DATA_GENERATOR = tasksDataGenerator;
+    global.TASKS_PATH = `${process.env.WEB_URL}/api/tasks`;
+    global.CREATE_TASK_PATH = `${process.env.WEB_URL}/api/tasks/create`;
 });
