dc3312dddb41143d121aff2ac4dca8cd52c0e698
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-02-07 16:50:48 -0500

    feat: create constants index to export various constants for HTTP status, pagination, tasks, user, and token purposes

---
+++ src/rules/constants/index.ts
@@ -0,0 +1,5 @@
+export * from './http-status-codes.constants';
+export * from './pagination.constants';
+export * from './tasks.constants';
+export * from './user.constants';
+export * from './token-purposes.constants';
