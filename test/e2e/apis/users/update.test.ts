9cef6b4c80eb4b652b99cc23e4ca07bdd8db1482
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-02-15 17:34:39 -0500

    test(e2e): add test to ensure role is downgraded when email is updated

--- test/e2e/apis/users/update.test.ts
+++ test/e2e/apis/users/update.test.ts
@@ -1,8 +1,37 @@
 import axios from "axios";
+import { ImapFlow } from "imapflow";
 import { handleAxiosError } from "../../helpers/handlers/axios-error.handler";
+import { getEmailConfirmationFromLink } from "../../helpers/email/get-email-confirmation-link-from-email-mssg.helper";
 
 describe('Update user', () => {
     describe('User successfully updated', () => {
+        let client: ImapFlow;
+
+        beforeAll(async () => {
+            client = new ImapFlow({
+                logger: false,
+                connectionTimeout: 10000,
+                host: 'imap.ethereal.email',
+                port: 993,
+                secure: true,
+                auth: {
+                    user: process.env.MAIL_SERVICE_USER!,
+                    pass: process.env.MAIL_SERVICE_PASS!,
+                }
+            });
+
+            await client.connect();
+        }, 11000);
+
+        afterAll(async () => {
+            await client.logout();
+        });
+
+        // close the inbox in order to refresh it the next time is opened
+        afterEach(async () => {
+            await client.mailboxClose();
+        });
+
         test('data should be updated when user is found', async () => {
             try {
                 // create user            
@@ -84,6 +113,63 @@ describe('Update user', () => {
             } catch (error) {
                 handleAxiosError(error);
             }
+        });
+
+        describe('Editor user update his email', () => {
+            test('should not be able to create a task because role was downgraded', async () => {
+                try {
+                    // create a user (readonly by default)
+                    const userCreated = await axios.post(global.REGISTER_USER_PATH, {
+                        name: global.DATA_GENERATOR.name(),
+                        email: global.DATA_GENERATOR.email(),
+                        password: global.DATA_GENERATOR.password()
+                    });
+
+                    const sessionToken = userCreated.data.token;
+                    const userEmail = userCreated.data.user.email;
+                    const userId = userCreated.data.user.id;
+
+                    // request email validation in order to upgrade to editor
+                    await axios.post(global.EMAIL_VALIDATION_PATH, null, {
+                        headers: {
+                            Authorization: `Bearer ${sessionToken}`
+                        }
+                    });
+
+                    // confirm email validation
+                    const confirmEmailUrl = await getEmailConfirmationFromLink(client, userEmail);
+                    await axios.get(confirmEmailUrl);
+
+                    // At this point, the user is an editor.
+                    // Update the email.
+                    await axios.patch(`${global.USERS_PATH}/${userId}`,
+                        { email: global.DATA_GENERATOR.email(), }, {
+                        headers: {
+                            Authorization: `Bearer ${sessionToken}`
+                        }
+                    });
+
+                    // attempt to create a task
+                    try {
+                        await axios.post(global.CREATE_TASK_PATH, {
+                            name: global.TASKS_DATA_GENERATOR.name(),
+                            description: global.TASKS_DATA_GENERATOR.description(),
+                            status: global.TASKS_DATA_GENERATOR.status(),
+                            priority: global.TASKS_DATA_GENERATOR.priority(),
+                        }, {
+                            headers: {
+                                Authorization: `Bearer ${sessionToken}`
+                            }
+                        });
+
+                    } catch (error: any) {
+                        expect(error.response.status).toBe(403);
+                    }
+
+                } catch (error) {
+                    handleAxiosError(error);
+                }
+            });
         });
     });
 });
